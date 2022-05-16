079df1494868251be79bce0436b575e56a29bd9c
Kelvin Bush <kelybush@gmail.com>
2022-05-16 21:24:57 +0000

    complete header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import MLogo from './assets/logo-dark.png';
+import searchIcon from './assets/search.svg';
+
+const logo = document.getElementById('logo');
+const myLogo = new Image();
+myLogo.src = MLogo;
+logo.appendChild(myLogo);
+
+const search = document.getElementById('search');
+search.src = searchIcon;
