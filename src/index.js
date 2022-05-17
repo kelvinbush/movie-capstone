b3e41769ce1fd6bfd9ed4a6dab15fcdfe2e90947
Kelvin Bush <kelybush@gmail.com>
2022-05-17 12:02:08 +0000

    Add image assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import MLogo from './assets/logo-light.png';
 import searchIcon from './assets/search.svg';
+import rating from './assets/rate.png';
+import loveIcon from './assets/love.png';
+import {getMovies} from "./modules/movieList";
 
 const logo = document.getElementById('logo');
 const myLogo = new Image();
@@ -9,3 +12,19 @@ logo.appendChild(myLogo);
 
 const search = document.getElementById('search');
 search.src = searchIcon;
+
+const moviesSection = document.getElementById('movies');
+moviesSection.innerHTML = getMovies().map(movie => {
+  return `
+    <article class="movie">
+      <img src="${movie.image.medium}" alt="${movie.name}">
+      <h3>${movie.name}</h3>
+      <p class="movie__genre">${movie.genres.join(', ')}</p>
+      <div>
+        <p class="movie__rating"><span><img src="${rating}" alt=""></span>${movie.rating.average}</p>
+        <p class="movie__likes"><img src="${loveIcon}" alt=""><span>15</span></p>
+      </div>
+      <button>Comments</button>
+    </article>
+  `
+}).join('');
