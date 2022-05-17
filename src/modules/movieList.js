41e1f2b46732c32977681d01a127ac773eae1b28
Kelvin Bush <kelybush@gmail.com>
2022-05-17 12:02:43 +0000

    set up static list of movies

---
+++ src/modules/movieList.js
@@ -0,0 +1,134 @@
+export const getMovies = () => {
+  return [
+    {
+      name: 'The Grinch',
+      genres: ['Comedy', 'Horror'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://image.tmdb.org/t/p/w300/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg',
+      },
+    },
+    {
+      name: 'The Black Panther',
+      genres: ['Action', 'Adventure', 'Fantasy'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://image.tmdb.org/t/p/w300/uxzzxijgPIY7slzFvMotPv8wjKA.jpg',
+      },
+    },
+    {
+      name: 'The Old Guard',
+      genres: ['Action', 'Adventure', 'Fantasy'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://upload.wikimedia.org/wikipedia/en/0/02/The_Old_Guard_2020_film_poster.png',
+      },
+    },
+    {
+      name: 'The Spiderman',
+      genres: ['Fiction', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://m.media-amazon.com/images/M/MV5BNDFkZDI5ZGUtYTdkOC00YTFiLWJjNjMtNTQ3ZjIxMTY2ZjMyXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_FMjpg_UX1000_.jpg',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://m.media-amazon.com/images/M/MV5BMDIyYzUwNjgtNDA1Mi00ZmQ3LThhNWQtN2RjZjczNjRlZmQ3XkEyXkFqcGdeQXVyNjMxNzcwOTI@._V1_.jpg',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*'
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      },
+    },
+    {
+      name: 'The Nun',
+      genres: ['Horror', 'Thriller'],
+      rating: {
+        average: '7.8',
+      },
+      image: {
+        medium: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-1642713480.jpeg?crop=1xw:1xh;center,top&resize=480:*',
+      }
+    }
+  ]
+}
